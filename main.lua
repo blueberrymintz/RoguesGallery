48d38cd73c5531cd1763cb565dae85569e620960
blueberrymintz <beelzebufo@icloud.com>
2025-06-07 14:01:43 -0700

    added scenery functionality! as well as starting up some scenes

--- main.lua
+++ main.lua
@@ -1,14 +1,22 @@
+anim8 = require 'libraries/anim8-master/anim8-master/anim8'
+local cron = require 'libraries/cronLua_Master/cron'
+local SceneryInit = require("path.to.scenery")
+local scenery = SceneryInit(
+    { path = "scenes/copier.lua", key = "example"},
+    {path = "scenes/ramboStart.lua", key = "ramboStart", default = true}
+
+)
+
 function love.load()
-
+    scenery:load()
 end
-
-
 
 function love.update(dt)
+    scenery:update(dt)
+end
 
+function love.draw()
+    scenery:draw()
 end
 
 
-function love.draw(dt)
-
-end
