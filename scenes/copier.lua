195fe579dc3bf08d10de7efde515e65cb2155801
blueberrymintz <beelzebufo@icloud.com>
2025-06-07 14:03:05 -0700

    initialized copier

--- scenes/copier.lua
+++ scenes/copier.lua
@@ -1,1 +1,15 @@
- 
+local game = {}
+
+function game:load()
+    print("Warp successful! Current Scene: EXAMPLE")
+end
+
+function game:update(dt)
+
+end
+
+function game:draw()
+
+end
+
+return game
